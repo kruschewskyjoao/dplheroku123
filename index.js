7ed3e380d9da9b1cbaf047b382c02106597bc82b
joaoKruschewsky <joaokruschewsky@gmail.com>
2022-07-22 12:08:21 -0300

    realizando deploy

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Escutando na porta ${port}</h1>`)
+});
+
+app.listen(port, () => console.log(`online ${port}`));
