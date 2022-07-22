7797d025bedcd067727a0dbbcdad8df313f2d5ed
joaoKruschewsky <joaokruschewsky@gmail.com>
2022-07-22 15:34:26 -0300

    teste

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Escutando na porta ${port}!!</h1>`);
+  res.send(`<h1>Escutando na porta ${port}!!!</h1>`);
 });
 
 app.listen(port, () => `online ${port}`);
